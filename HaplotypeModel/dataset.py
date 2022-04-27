05e30a807bcb6be35d50b0c14d642f43cff8058f
huangneng <nhuang94@qq.com>
2022-04-27 08:39:30 +0800

    remove unused package

--- HaplotypeModel/dataset.py
+++ HaplotypeModel/dataset.py
@@ -3,12 +3,10 @@ from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
 import torch
 from tqdm import tqdm
-import h5py
 import numpy as np
 import pandas as pd
 import argparse
 import os
-import sys
 import tables
 from get_truth import GT21_LABLES
 
