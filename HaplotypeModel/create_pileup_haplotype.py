3d2c7e58bf75d6b1a8f19714186399ce41ebf72c
huangnengCSU <huangneng@csu.edu.cn>
2022-08-27 10:39:04 +0800

    get feature from phased bam without segmentation

---
+++ HaplotypeModel/create_pileup_haplotype.py
@@ -0,0 +1,228 @@
+import numpy as np
+import pysam
+import sys
+import pandas as pd
+from itertools import product
+
+base_to_int = {'A': 1, 'C': 2, 'G': 3, 'T': 4}
+int_to_base = {1: 'A', 2: 'C', 3: 'G', 4: 'T', -1: 'D', 0: 'B'}
+edges_label = list(map(''.join, product(
+    ['A', 'C', 'G', 'T', 'D'], ['A', 'C', 'G', 'T', 'D'])))
+
+
+class Edge:
+    def __init__(self, s, t):
+        self.source = s
+        self.target = t
+        self.weight = 1
+
+    def add_weight(self, w):
+        self.weight += w
+
+
+def single_group_pileup_haplotype_feature(samfile, groups, max_coverage, adjacent_size, pileup_flanking_size):
+    candidate_positions = []
+    haplotype_positions, haplotype_sequences, haplotype_baseq, haplotype_mapq = [], [], [], []
+    haplotype_hap, pileup_hap = [], []
+    pileup_sequences, pileup_baseq, pileup_mapq = [], [], []
+    max_haplotype_depth, max_pileup_depth = 0, 0
+    haplotype_depths = []
+    pileup_depths = []
+    positions = []
+    ctgname = groups[0][0].ctgname
+    for g in groups:
+        assert g[0].ctgname == ctgname
+        for snpitem in g:
+            positions.append(int(snpitem.position))
+    positions = sorted(list(set(positions)))
+    failed_positions = []
+    for pileupcolumn in samfile.pileup(ctgname, positions[0], positions[-1], min_base_quality=0, min_mapping_quality=0):
+        if pileupcolumn.pos + 1 < positions[0]:
+            continue
+        if pileupcolumn.pos + 1 > positions[-1]:
+            break
+        if pileupcolumn.pos + 1 not in positions:
+            continue
+        if pileupcolumn.n > max_coverage:
+            failed_positions.append(pileupcolumn.pos + 1)
+
+    # 存在某些位点覆盖度超过阈值，则过滤掉这些group
+    if len(failed_positions) > 0:
+        new_groups = []
+        for g in groups:
+            failed = False
+            for snpitem in g:
+                if snpitem.position in failed_positions:
+                    failed = True
+                    break
+            if not failed:
+                new_groups.append(g)
+        groups = new_groups
+
+    # TODO: 实际数据中，会存在过滤后的group为空的情况，如何处理？
+    if len(groups) == 0:
+        print("No group is left after filtering")
+        return candidate_positions,  haplotype_positions, haplotype_sequences, haplotype_baseq, haplotype_mapq, haplotype_hap, \
+               max_haplotype_depth, pileup_sequences, pileup_baseq, pileup_mapq, pileup_hap, max_pileup_depth
+
+    try:
+        """
+        extend positions 存储了每个候选位点周围11mer的position，以及候选位点周围高质量Hete位点的position
+        """
+        extend_positions = []
+        for g in groups:
+            for tidx in range(len(g)):
+                snpitem = g[tidx]
+                if tidx == len(g) // 2:
+                    ## pileup window size = 2 * pileup_flanking_size + 1
+                    extend_positions.extend(
+                        list(range(int(snpitem.position) - pileup_flanking_size, int(snpitem.position) + pileup_flanking_size+1)))  # pileup的position
+                else:
+                    extend_positions.append(int(snpitem.position))  # haplotype的position
+        extend_positions = sorted(list(set(extend_positions)))
+
+        SNP_mat = {}
+        hap_mat = {}
+        base_quality_mat = {}
+        mapping_quality_mat = {}
+        for pileupcolumn in samfile.pileup(ctgname, extend_positions[0], extend_positions[-1], min_base_quality=0,
+                                           min_mapping_quality=0):
+            if pileupcolumn.pos + 1 < extend_positions[0]:
+                continue
+            if pileupcolumn.pos + 1 > extend_positions[-1]:
+                break
+            if pileupcolumn.pos + 1 not in extend_positions:
+                continue
+            assert pileupcolumn.n <= max_coverage
+            i = extend_positions.index(pileupcolumn.pos + 1)
+            # print("coverage at base %s = %s" % (pileupcolumn.pos, pileupcolumn.n))
+            for pileupread in pileupcolumn.pileups:
+                qname = pileupread.alignment.query_name
+                tag = 3 # unphased tag
+                if pileupread.alignment.has_tag("HP"):
+                    tag = pileupread.alignment.get_tag("HP")    # if tag exists, it equals to 1 or 2.
+                assert tag == 1 or tag == 2 or tag == 3
+                if qname not in SNP_mat.keys():
+                    SNP_mat[qname] = [0] * len(extend_positions)
+                    hap_mat[qname] = [0] * len(extend_positions)
+                    base_quality_mat[qname] = [0] * len(extend_positions)
+                    mapping_quality_mat[qname] = [0] * len(extend_positions)
+                    row = SNP_mat[qname]
+                    hp = hap_mat[qname]
+                    bq_row = base_quality_mat[qname]
+                    mq_row = mapping_quality_mat[qname]
+                else:
+                    row = SNP_mat[qname]
+                    hap = hap_mat[qname]
+                    bq_row = base_quality_mat[qname]
+                    mq_row = mapping_quality_mat[qname]
+                if not pileupread.is_del and not pileupread.is_refskip:
+                    qbase = pileupread.alignment.query_sequence[pileupread.query_position]
+                    row[i] = base_to_int[str.upper(qbase)]
+                    hp[i] = tag
+                    base_q = pileupread.alignment.query_qualities[pileupread.query_position]
+                    bq_row[i] = base_q
+                    map_q = pileupread.alignment.mapping_quality
+                    mq_row[i] = map_q
+                elif pileupread.is_del:
+                    # base is -1, base_q is 0, and map_q is normal.
+                    row[i] = -1  # deletion
+                    hp[i] = tag
+                    map_q = pileupread.alignment.mapping_quality
+                    mq_row[i] = map_q
+        df = pd.DataFrame(SNP_mat, index=extend_positions).T
+        hap_df = pd.DataFrame(hap_mat, index=extend_positions).T
+        baseq_df = pd.DataFrame(base_quality_mat, index=extend_positions).T
+        mapq_df = pd.DataFrame(mapping_quality_mat, index=extend_positions).T
+
+        for g in groups:
+            ctg = g[0].ctgname
+            gpos = [int(snpitem.position) for snpitem in g]
+            gdf = df[gpos]
+            # filter gdf: 每条reads必须跨过group的中的中间位置即可。如果要求reads跨过所有点，则筛选出来的覆盖度很低
+            filter_gdf_lines = []
+            for i in range(gdf.shape[0]):
+                mid_gpos = gpos[len(gpos) // 2]
+                if gdf.iloc[i][mid_gpos] != 0:
+                    filter_gdf_lines.append(i)
+                # if gdf.iloc[i][gpos[0]] != 0 and gdf.iloc[i][gpos[-1]] != 0:
+                #     filter_gdf_lines.append(i)
+
+            haplotype_read_df_gpos = df[gpos].iloc[filter_gdf_lines]  # [depth, adjacent_size*2+1]
+            haplotype_hap_df_gpos = hap_df[gpos].iloc[filter_gdf_lines]   # [depth, adjacent_size*2+1]
+            haplotype_baseq_df_gpos = baseq_df[gpos].iloc[filter_gdf_lines]  # [depth, adjacent_size*2+1]
+            haplotype_mapq_df_gpos = mapq_df[gpos].iloc[filter_gdf_lines]  # [depth, adjacent_size*2+1]
+
+            ###: 增加四个df根据hap排序,根据中间位置的hap值进行排序
+            mid_col_name = haplotype_hap_df_gpos.columns[len(haplotype_hap_df_gpos.columns) // 2]
+            sort_haplotype_hap_df_gpos = haplotype_hap_df_gpos.sort_values(by=mid_col_name)
+            rearrange_index = sort_haplotype_hap_df_gpos.index
+            haplotype_read_df_gpos = haplotype_read_df_gpos.loc[rearrange_index]
+            haplotype_hap_df_gpos = haplotype_hap_df_gpos.loc[rearrange_index]
+            haplotype_baseq_df_gpos = haplotype_baseq_df_gpos.loc[rearrange_index]
+            haplotype_mapq_df_gpos = haplotype_mapq_df_gpos.loc[rearrange_index]
+
+
+            candidate_positions.append(ctg + ":" + str(gpos[len(gpos) // 2]))
+            haplotype_positions.append([ctg + ":" + str(gpos_v) for gpos_v in gpos])
+            haplotype_sequences.append(haplotype_read_df_gpos.values)
+            haplotype_hap.append(haplotype_hap_df_gpos.values)
+            haplotype_baseq.append(haplotype_baseq_df_gpos.values)
+            haplotype_mapq.append(haplotype_mapq_df_gpos.values)
+            haplotype_depths.append(haplotype_read_df_gpos.shape[0])
+
+            # 候选位点周围11-mer的reads比对信息
+            gpos = list(range(int(g[len(g) // 2].position) - pileup_flanking_size, int(g[len(g) // 2].position) + pileup_flanking_size + 1))
+            gdf = df[gpos]
+            # filter gdf: 每条reads必须跨过group的中的中间位置即可。如果要求reads跨过所有点，则筛选出来的覆盖度很低
+            ### 此处的gdf与haplotype的gdf不同了
+            filter_gdf_lines = []
+            for i in range(gdf.shape[0]):
+                mid_gpos = gpos[len(gpos) // 2]
+                if gdf.iloc[i][mid_gpos] != 0:
+                    filter_gdf_lines.append(i)
+                # if gdf.iloc[i][gpos[0]] != 0 and gdf.iloc[i][gpos[-1]] != 0:
+                #     filter_gdf_lines.append(i)
+            pileup_read_df_gpos = df[gpos].iloc[filter_gdf_lines]  # [depth, 11]
+            pileup_hap_df_gpos = hap_df[gpos].iloc[filter_gdf_lines]    # [depth, 11]
+            pileup_baseq_df_gpos = baseq_df[gpos].iloc[filter_gdf_lines]  # [depth, 11]
+            pileup_mapq_df_gpos = mapq_df[gpos].iloc[filter_gdf_lines]  # [depth, 11]
+
+            ###: 增加四个df根据hap排序,根据中间位置的hap值进行排序
+            mid_col_name = pileup_hap_df_gpos.columns[len(pileup_hap_df_gpos.columns) // 2]
+            sort_pileup_hap_df_gpos = pileup_hap_df_gpos.sort_values(by=mid_col_name)
+            rearrange_index = sort_pileup_hap_df_gpos.index
+            pileup_read_df_gpos = pileup_read_df_gpos.loc[rearrange_index]
+            pileup_hap_df_gpos = pileup_hap_df_gpos.loc[rearrange_index]
+            pileup_baseq_df_gpos = pileup_baseq_df_gpos.loc[rearrange_index]
+            pileup_mapq_df_gpos = pileup_mapq_df_gpos.loc[rearrange_index]
+
+
+            pileup_sequences.append(pileup_read_df_gpos.values)
+            pileup_hap.append(pileup_hap_df_gpos.values)
+            pileup_baseq.append(pileup_baseq_df_gpos.values)
+            pileup_mapq.append(pileup_mapq_df_gpos.values)
+            pileup_depths.append(pileup_read_df_gpos.shape[0])
+
+        max_haplotype_depth = max(haplotype_depths)
+        max_pileup_depth = max(pileup_depths)
+        return candidate_positions,  haplotype_positions, haplotype_sequences, haplotype_baseq, haplotype_mapq, haplotype_hap, \
+               max_haplotype_depth, pileup_sequences, pileup_baseq, pileup_mapq, pileup_hap, max_pileup_depth
+    except:
+        print("try..except.. break")
+        return candidate_positions,  haplotype_positions, haplotype_sequences, haplotype_baseq, haplotype_mapq, haplotype_hap, \
+               max_haplotype_depth, pileup_sequences, pileup_baseq, pileup_mapq, pileup_hap, max_pileup_depth
+
+
+if __name__ == "__main__":
+    bamfile_path = sys.argv[1]
+    ctg = input("please input the contig name:\n")
+    positions = input("please input the positions on the contig:\n")
+    positions = [int(v) for v in positions.strip().split(' ')]
+    samfile = pysam.AlignmentFile(bamfile_path, 'r')
+    # edge_dict = extract_pileups(samfile, ctg, positions)
+    # with open('graph.csv', 'w') as fout:
+    #     fout.write('source,target,weight\n')
+    #     for k in edge_dict.keys():
+    #         edge = edge_dict[k]
+    #         fout.write('%s,%s,%d\n' % (edge.source, edge.target, edge.weight))
---
+++ HaplotypeModel/make_predict_bins.py
@@ -0,0 +1,270 @@
+import os
+import sys
+import time
+import math
+import tables
+import numpy as np
+import pysam
+import datetime
+import concurrent.futures
+from tqdm import tqdm
+from argparse import ArgumentParser
+# from extract_adjacent_pileup import extract_pileups, extract_pileups_batch
+from select_hetesnp_homosnp import select_snp
+from create_pileup_haplotype import single_group_pileup_haplotype_feature
+
+
+class SNPItem:
+    def __init__(self, ctgname, pos, homo_hete, info=None):
+        self.ctgname = ctgname
+        self.position = pos
+        self.homo_hete = homo_hete
+        self.info = info
+
+    def is_heterozygous(self):
+        if self.homo_hete == '0/1' or self.homo_hete == '0|1' or self.homo_hete == '1/2' or self.homo_hete == '1|2':
+            return True
+        else:
+            return False
+
+
+class GroupQueue:
+    def __init__(self, maxsize):
+        self.maxsize = maxsize
+        self.__queue = []
+
+    def full(self):
+        return len(self.__queue) == self.maxsize
+
+    def empty(self):
+        return len(self.__queue) == 0
+
+    def qsize(self):
+        return len(self.__queue)
+
+    def get(self):
+        tmp = self.__queue[0]
+        self.__queue = self.__queue[1:]
+        return tmp
+
+    def put(self, item):
+        if self.full():
+            self.get()
+        self.__queue.append(item)
+
+    def show_middle(self):
+        if self.full():
+            return self.__queue[self.maxsize // 2]
+        else:
+            return None
+
+    def show_last(self):
+        if not self.empty():
+            return self.__queue[self.qsize() - 1]
+        else:
+            return None
+
+    def show_queue(self):
+        q = self.__queue
+        return q
+
+    def empty_queue(self):
+        self.__queue = []
+
+
+def multigroups_pileup_haplotype_feature(bam, groups, max_coverage, adjacent_size, pileup_flanking_size):
+    ctgname = groups[0][0].ctgname
+    out_candidate_positions = []
+    out_haplotype_hap, out_pileup_hap = [], []
+    out_haplotype_positions, out_haplotype_sequences, out_haplotype_baseq, out_haplotype_mapq = [], [], [], []
+    out_pileup_sequences, out_pileup_baseq, out_pileup_mapq = [], [], []
+    max_haplotype_depth, max_pileup_depth = 0, 0
+    samfile = pysam.AlignmentFile(bam, 'r')
+    # 对groups再进行分组sub_group，每个sub_group包含100个group，如果某个group与前一个group相距远，则直接中断，新起sub_group
+    dt = 0
+    step = 100
+    # sub_groups = [groups[dt:dt + step] for dt in range(0, len(groups), step)]  # each time pysam pileup processes multiple groups for acceleration
+    sub_groups = []
+    num_groups = len(groups)
+    while dt < num_groups:
+        for i in range(1, step):
+            if dt + i >= num_groups:
+                sub_groups.append(groups[dt:dt + i])
+                dt = dt + i
+                break
+            else:
+                if int(groups[dt + i][0].position) - int(groups[dt + i - 1][-1].position) > 1000:
+                    sub_groups.append(groups[dt:dt + i])
+                    dt = dt + i
+                    break
+                elif i == step - 1:
+                    sub_groups.append(groups[dt:dt + i + 1])
+                    dt = dt + i + 1
+                    break
+                else:
+                    pass
+    for sub_sub_groups in sub_groups:
+        candidate_positions, haplotype_positions, haplotype_sequences, haplotype_baseq, haplotype_mapq, haplotype_hap, haplotype_depth, pileup_sequences, pileup_baseq, \
+        pileup_mapq, pileup_hap, pileup_depth = single_group_pileup_haplotype_feature(samfile, sub_sub_groups, max_coverage, adjacent_size, pileup_flanking_size)
+        if all(rtv is not None for rtv in [candidate_positions, haplotype_positions, haplotype_sequences, haplotype_baseq, haplotype_mapq, haplotype_depth, pileup_sequences, pileup_baseq, pileup_mapq, pileup_depth, haplotype_hap, pileup_hap]):
+            out_candidate_positions.extend(candidate_positions)
+            out_haplotype_positions.extend(haplotype_positions)
+            out_haplotype_sequences.extend(haplotype_sequences)
+            out_haplotype_hap.extend(haplotype_hap)
+            out_haplotype_baseq.extend(haplotype_baseq)
+            out_haplotype_mapq.extend(haplotype_mapq)
+            max_haplotype_depth = haplotype_depth if haplotype_depth > max_haplotype_depth else max_haplotype_depth
+            out_pileup_sequences.extend(pileup_sequences)
+            out_pileup_hap.extend(pileup_hap)
+            out_pileup_baseq.extend(pileup_baseq)
+            out_pileup_mapq.extend(pileup_mapq)
+            max_pileup_depth = pileup_depth if pileup_depth > max_pileup_depth else max_pileup_depth
+        else:
+            print("single_group_pileup_haplotype_feature output is None")
+    return out_candidate_positions, out_haplotype_positions, out_haplotype_sequences, out_haplotype_baseq, \
+           out_haplotype_mapq, out_haplotype_hap, max_haplotype_depth, out_pileup_sequences, out_pileup_baseq, out_pileup_mapq, out_pileup_hap, max_pileup_depth
+
+
+def Run(args):
+    pileup_vcf = args.pileup_vcf
+    low_quality_threshold = args.low_quality_threshold
+    adjacent_size = args.adjacent_size
+    pileup_flanking_size = args.pileup_flanking_size
+    hete_support_quality = args.hete_support_quality
+
+    groups_dict = select_snp(vcf_file=pileup_vcf, quality_threshold=low_quality_threshold, adjacent_size=adjacent_size, support_quality=hete_support_quality)
+    # 把groups先按照染色体进行划分
+    for k in groups_dict.keys():
+        ## k is contig name
+        out_candidate_positions = []
+        out_haplotype_hap, out_pileup_hap = [], []
+        out_haplotype_positions, out_haplotype_sequences, out_haplotype_baseq, out_haplotype_mapq = [], [], [], []
+        out_pileup_sequences, out_pileup_baseq, out_pileup_mapq = [], [], []
+        max_pileup_depth, max_haplotype_depth = 0, 0
+        chromosome_groups = groups_dict[k]
+        total_threads = args.threads
+        step = math.ceil(len(chromosome_groups) / total_threads)
+        divided_groups = [chromosome_groups[dt:dt + step] for dt in range(0, len(chromosome_groups), step)]  # divided for multiple threads
+
+        with concurrent.futures.ProcessPoolExecutor(max_workers=total_threads) as executor:
+            signals = [executor.submit(multigroups_pileup_haplotype_feature, args.bam, groups, args.max_coverage, adjacent_size, pileup_flanking_size) for groups in divided_groups]
+            for sig in concurrent.futures.as_completed(signals):
+                if sig.exception() is None:
+                    # get the results
+                    [candidate_positions, haplotype_positions, haplotype_sequences, haplotype_baseq, haplotype_mapq, haplotype_hap, haplotype_depth,
+                     pileup_sequences, pileup_baseq, pileup_mapq, pileup_hap, pileup_depth] = sig.result()
+                    if all( len(rtv)>0 for rtv in [candidate_positions, haplotype_positions, haplotype_sequences, haplotype_baseq, haplotype_mapq, haplotype_hap, haplotype_depth,
+                     pileup_sequences, pileup_baseq, pileup_mapq, pileup_hap, pileup_depth]):
+                        out_candidate_positions.extend(candidate_positions)
+                        out_haplotype_positions.extend(haplotype_positions)
+                        out_haplotype_sequences.extend(haplotype_sequences)
+                        out_haplotype_hap.extend(haplotype_hap)
+                        out_haplotype_baseq.extend(haplotype_baseq)
+                        out_haplotype_mapq.extend(haplotype_mapq)
+                        max_haplotype_depth = haplotype_depth if haplotype_depth > max_haplotype_depth else max_haplotype_depth
+                        out_pileup_sequences.extend(pileup_sequences)
+                        out_pileup_hap.extend(pileup_hap)
+                        out_pileup_baseq.extend(pileup_baseq)
+                        out_pileup_mapq.extend(pileup_mapq)
+                        max_pileup_depth = pileup_depth if pileup_depth > max_pileup_depth else max_pileup_depth
+                    else:
+                        print("multicandidates_pileup_haplotype_feature output is empty")
+                else:
+                    sys.stderr.write("ERROR: " + str(sig.exception()) + "\n")
+                sig._result = None  # python issue 27144
+
+            # 排序
+        out_candidate_positions = np.array(out_candidate_positions)
+        new_candidate_positions = [int(v.split(':')[1]) for v in out_candidate_positions]
+        new_index = np.argsort(new_candidate_positions)
+        out_candidate_positions = out_candidate_positions[new_index]
+        out_haplotype_positions = np.array(out_haplotype_positions)[new_index]
+
+        out_haplotype_sequences = [np.expand_dims(np.pad(a, ((0, max_haplotype_depth - a.shape[0]), (0, 0)), 'constant', constant_values=-2), 0) for a in out_haplotype_sequences]
+        out_haplotype_sequences = np.concatenate(out_haplotype_sequences)[new_index]
+
+        out_haplotype_hap = [np.expand_dims(np.pad(a, ((0, max_haplotype_depth - a.shape[0]), (0, 0)), 'constant', constant_values=-2), 0) for a in out_haplotype_hap]
+        out_haplotype_hap = np.concatenate(out_haplotype_hap)[new_index]
+
+        out_haplotype_baseq = [np.expand_dims(np.pad(a, ((0, max_haplotype_depth - a.shape[0]), (0, 0)), 'constant', constant_values=-2), 0) for a in out_haplotype_baseq]
+        out_haplotype_baseq = np.concatenate(out_haplotype_baseq)[new_index]
+
+        out_haplotype_mapq = [np.expand_dims(np.pad(a, ((0, max_haplotype_depth - a.shape[0]), (0, 0)), 'constant', constant_values=-2), 0) for a in out_haplotype_mapq]
+        out_haplotype_mapq = np.concatenate(out_haplotype_mapq)[new_index]
+
+        out_pileup_sequences = [np.expand_dims(np.pad(a, ((0, max_pileup_depth - a.shape[0]), (0, 0)), 'constant', constant_values=-2), 0) for a in out_pileup_sequences]
+        out_pileup_sequences = np.concatenate(out_pileup_sequences)[new_index]
+
+        out_pileup_hap = [np.expand_dims(np.pad(a, ((0, max_pileup_depth - a.shape[0]), (0, 0)), 'constant', constant_values=-2), 0) for a in out_pileup_hap]
+        out_pileup_hap = np.concatenate(out_pileup_hap)[new_index]
+
+        out_pileup_baseq = [np.expand_dims(np.pad(a, ((0, max_pileup_depth - a.shape[0]), (0, 0)), 'constant', constant_values=-2), 0) for a in out_pileup_baseq]
+        out_pileup_baseq = np.concatenate(out_pileup_baseq)[new_index]
+
+        out_pileup_mapq = [np.expand_dims(np.pad(a, ((0, max_pileup_depth - a.shape[0]), (0, 0)), 'constant', constant_values=-2), 0) for a in out_pileup_mapq]
+        out_pileup_mapq = np.concatenate(out_pileup_mapq)[new_index]
+
+        TABLE_FILTERS = tables.Filters(complib='blosc:lz4hc', complevel=5)
+        output = args.output + '/' + k + '.bin'
+        table_file = tables.open_file(output, mode='w')
+        int_atom = tables.Atom.from_dtype(np.dtype('int32'))
+        string_atom = tables.StringAtom(itemsize=30 * (2 * adjacent_size))
+        table_file.create_earray(where='/', name='haplotype_sequences', atom=int_atom, shape=[0, max_haplotype_depth, 2 * adjacent_size + 1])
+        table_file.create_earray(where='/', name='haplotype_hap', atom=int_atom, shape=[0, max_haplotype_depth, 2 * adjacent_size + 1])
+        table_file.create_earray(where='/', name='haplotype_baseq', atom=int_atom, shape=[0, max_haplotype_depth, 2 * adjacent_size + 1])
+        table_file.create_earray(where='/', name='haplotype_mapq', atom=int_atom, shape=[0, max_haplotype_depth, 2 * adjacent_size + 1])
+        table_file.create_earray(where='/', name='pileup_sequences', atom=int_atom, shape=[0, max_pileup_depth, 2 * pileup_flanking_size + 1])
+        table_file.create_earray(where='/', name='pileup_hap', atom=int_atom, shape=[0, max_pileup_depth, 2 * pileup_flanking_size + 1])
+        table_file.create_earray(where='/', name='pileup_baseq', atom=int_atom, shape=[0, max_pileup_depth, 2 * pileup_flanking_size + 1])
+        table_file.create_earray(where='/', name='pileup_mapq', atom=int_atom, shape=[0, max_pileup_depth, 2 * pileup_flanking_size + 1])
+        table_file.create_earray(where='/', name='candidate_positions', atom=string_atom, shape=(0, 1), filters=TABLE_FILTERS)
+        table_file.create_earray(where='/', name='haplotype_positions', atom=string_atom, shape=(0, adjacent_size * 2 + 1),filters=TABLE_FILTERS)
+
+
+        table_file.root.haplotype_sequences.append(out_haplotype_sequences)
+        table_file.root.haplotype_hap.append(out_haplotype_hap)
+        table_file.root.haplotype_baseq.append(out_haplotype_baseq)
+        table_file.root.haplotype_mapq.append(out_haplotype_mapq)
+        table_file.root.pileup_sequences.append(out_pileup_sequences)
+        table_file.root.pileup_hap.append(out_pileup_hap)
+        table_file.root.pileup_baseq.append(out_pileup_baseq)
+        table_file.root.pileup_mapq.append(out_pileup_mapq)
+        table_file.root.candidate_positions.append(np.array(out_candidate_positions).reshape(-1, 1))
+        table_file.root.haplotype_positions.append(np.array(out_haplotype_positions).reshape(-1, adjacent_size * 2 + 1))
+        table_file.close()
+    print(time.strftime("[%a %b %d %H:%M:%S %Y] Done.", time.localtime()))
+
+
+def main():
+    parser = ArgumentParser(description="Create pileup and haplotype feature of candidate SNPs for predicting")
+    parser.add_argument("--pileup_vcf", type=str, required=True,
+                        help="Input the variants called by pileup model, required.")
+    parser.add_argument("--bam", type=str, required=True,
+                        help="Input the bam file, required.")
+    parser.add_argument("--reference", type=str, required=True,
+                        help="Input the reference file, required.")
+    parser.add_argument("--output", type=str, required=True,
+                        help="Input the output directory, required.")
+    parser.add_argument("--pileup_flanking_size", type=int, default=5,
+                        help="The flanking size used to construct the pileup feature of the candidate SNP , default: %(default)d")                  
+    parser.add_argument("--adjacent_size", type=int, default=5,
+                        help="The range of SNPS around the candidate sites to be considered as groups, default: %(default)d")
+    parser.add_argument("--low_quality_threshold", type=int, default=19,
+                        help="The low quality threshold of snp site for haplotype model.")
+    parser.add_argument('--hete_support_quality', default=14, type=float,
+                        help='Min quality of hetezygous SNP used for making up of group. (default: %(default)f)')
+    parser.add_argument("--max_coverage", type=int, default=150,
+                        help="The maximum coverage of each position, default: %(default)d")
+    parser.add_argument("--threads", '-t', type=int, default=1,
+                        help="The number of threads used for computing: %(default)d")
+    args = parser.parse_args()
+    if len(sys.argv[1:]) == 0:
+        parser.print_help()
+        sys.exit(1)
+    if not os.path.exists(args.output):
+        os.mkdir(args.output)
+    Run(args)
+
+
+if __name__ == "__main__":
+    main()
